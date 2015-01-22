73dd4a6c843e53c955684a10753370867f03b8b7
worace <horace.d.williams@gmail.com>
2015-01-21 23:16:00 -0700

    trying with multiple goroutines

--- miner.go
+++ miner.go
@@ -9,6 +9,8 @@ import (
 	"math/rand"
 	"net/http"
 	"net/url"
+	"runtime"
+	"time"
 )
 
 const baseUrl string = "http://git-coin.herokuapp.com"
@@ -18,7 +20,7 @@ func gcUrl(path string) string {
 }
 
 func generateMessage() string {
-	return fmt.Sprintf("%d", rand.Int())
+	return fmt.Sprintf("%d%v", rand.Int63(), time.Now())
 }
 
 func digest(input string) []byte {
@@ -55,27 +57,42 @@ func submitMessage(message string) {
 	fmt.Println(string(body))
 }
 
-func main() {
+func mine(finished chan bool) {
 	iterations := 1
 	currentTarget := fetchTarget()
 	targetBytes, _ := hex.DecodeString(string(currentTarget))
 	reloadTarget := func() {
 		currentTarget = fetchTarget()
 		targetBytes, _ = hex.DecodeString(string(currentTarget))
+		fmt.Println("target now: ", string(currentTarget))
 		iterations = 1
 	}
 	for {
 		iterations++
 		message := generateMessage()
 		hashAttempt := digest(message)
-		if iterations > 1000000 {
-			fmt.Println("completed 1mil attempts; re-checking target")
+		if iterations > 4000000 {
+			fmt.Println("completed 4mil attempts; re-checking target")
 			reloadTarget()
 		}
 		if bytes.Compare(hashAttempt, targetBytes) < 0 {
 			fmt.Println("congrats got a hash!")
 			submitMessage(message)
 			reloadTarget()
+			//finished <- true
 		}
 	}
+}
+
+func main() {
+	fmt.Println("NUM CPUS", runtime.NumCPU())
+	finished := make(chan bool)
+
+	fmt.Println()
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go mine(finished)
+	}
+	foundHash := <-finished
+	fmt.Println("Found hash", foundHash)
 }
