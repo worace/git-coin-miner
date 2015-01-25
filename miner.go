2dbc79344b34b76b79d6fa7f413bf6b05d2915b2
worace <horace.d.williams@gmail.com>
2015-01-24 21:57:26 -0700

    experimenting with fetching targets in sep routine and sending results with channels

--- miner.go
+++ miner.go
@@ -13,9 +13,8 @@ import (
 	"time"
 )
 
-const baseUrl string = "http://git-coin.herokuapp.com"
-
-//const baseUrl string = "http://localhost:9292"
+//const baseUrl string = "http://git-coin.herokuapp.com"
+const baseUrl string = "http://localhost:9292"
 
 func gcUrl(path string) string {
 	return fmt.Sprintf("%s%s", baseUrl, path)
@@ -36,6 +35,7 @@ func fetchTarget() []byte {
 		panic("error reading response body")
 	}
 	defer resp.Body.Close()
+	target, _ = hex.DecodeString(string(target))
 	return target
 }
 
@@ -59,45 +59,77 @@ func digest(input string) []byte {
 	return h.Sum(nil)
 }
 
-func mine(finished chan bool) {
+func mine(target []byte, newTarget chan []byte, incomingTargets chan []byte) {
 	iterations := 1
-	currentTarget := fetchTarget()
-	targetBytes, _ := hex.DecodeString(string(currentTarget))
-	reloadTarget := func() {
-		currentTarget = fetchTarget()
-		targetBytes, _ = hex.DecodeString(string(currentTarget))
-		fmt.Println("target now: ", string(currentTarget))
-		iterations = 1
-	}
+	currentTarget := target
 	message := generateMessage()
-	start := time.Now()
 	for {
-		iterations++
-		hashAttempt := digest(message)
-		if iterations > 4000000 {
-			fmt.Println("completed 4mil attempts; re-checking target; current message: ", message)
-			fmt.Println("elapsed time: ", time.Now().Sub(start))
-			start = time.Now()
-			fmt.Printf("digest: ", hex.EncodeToString(hashAttempt))
-			reloadTarget()
-			iterations = 1
+		select {
+		case t := <-incomingTargets:
+			fmt.Println("received new target from parent", t)
+			currentTarget = t
+			fmt.Println("worker finished inc target")
+		default:
+			//fmt.Println("worker looping")
+			iterations++
+			hashAttempt := digest(message)
+			if iterations > 1000000 {
+				fmt.Println("completed 4mil attempts; asking parrent to read target; current message: ", message)
+				fmt.Println("queue size", len(newTarget))
+				newTarget <- make([]byte, 0, 0)
+				//reloadTarget()
+				iterations = 1
+			}
+			if bytes.Compare(hashAttempt, currentTarget) < 0 {
+				newTarget <- currentTarget
+				fmt.Println("congrats got a hash! submit target", hashAttempt, message)
+				submitMessage(message)
+				//reloadTarget()
+			}
+			message = hex.EncodeToString(hashAttempt)
+			//fmt.Println("worker finished default")
 		}
-		if bytes.Compare(hashAttempt, targetBytes) < 0 {
-			fmt.Println("congrats got a hash!")
-			submitMessage(message)
-			reloadTarget()
-			//finished <- true
-		}
-		message = hex.EncodeToString(hashAttempt)
 	}
 }
 
 func main() {
 	fmt.Println("NUM CPUS", runtime.NumCPU())
 	finished := make(chan bool)
+	cpus := runtime.NumCPU()
+	newTarget := make(chan []byte, 100)
+	listeners := make([]chan []byte, 0)
+	target := fetchTarget()
 
-	for i := 0; i < runtime.NumCPU(); i++ {
-		go mine(finished)
+	for i := 0; i < cpus; i++ {
+		incTargets := make(chan []byte)
+		listeners = append(listeners, incTargets)
+		go mine(target, newTarget, incTargets)
+	}
+
+	for {
+		select {
+		case t := <-newTarget:
+			if len(t) > 0 {
+				fmt.Println("new target received at main, should send it to workers", t)
+				t = fetchTarget()
+				for i, l := range listeners {
+					fmt.Println("parent will pre-fetched target to listener", i)
+					l <- t
+				}
+			} else {
+				fmt.Println("received empty target; need to update ourselves")
+				fmt.Println(len(listeners))
+				fmt.Println(listeners)
+				t = fetchTarget()
+				for i, l := range listeners {
+					fmt.Println("parent will send target to listener", i)
+					l <- t
+				}
+			}
+			fmt.Println("parent finished newTarget")
+		default:
+			//fmt.Println("main looping")
+		}
 	}
 	foundHash := <-finished
 	fmt.Println("Found hash", foundHash)
